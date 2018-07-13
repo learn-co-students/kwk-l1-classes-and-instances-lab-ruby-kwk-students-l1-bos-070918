dc636f8ffe77b25e2b089a58116c044dae511d59
Sidneyyy <catitude14@outlook.com>
2018-07-13 19:10:11 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,17 @@
 # dog.rb
+
+class Dog
+  
+  attr_accessor :name , :breed
+  
+end
+
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
+puts fido.name = "Fido"
+puts fido.breed = "Husky"
+puts snoopy.name = "Snoopy"
+puts snoopy.breed = "Labrador"
+puts lassie.name = "Lassie"
+puts lassie.breed = "Border Collie"
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,18 @@
 # person.rb
+
+class Person
+  
+  def name=(name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+  
+end
+
+adele_goldberg = Person.new
+alan_kay = Person.new
+puts adele_goldberg.name = "Adele Goldberg"
+puts alan_kay.name = "Alan Kay"
